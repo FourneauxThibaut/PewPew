15bfed1d0110cfdb82312f4f7dade1a7271e81ec
fourneauxthibaut <fourneaux.thibaut@gmail.com>
2021-10-08 14:15:58 +0200

    modification into classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,106 @@
-var targets = []; //hold all the targets
 let canvas = document.getElementById("mon_canvas");
 let ctx = canvas.getContext("2d");
-let backgroundPos = -2500;
+let gameOver = false;
+let backgroundPos = -3000;
 let mouse = {
     x: undefined, 
     y:undefined
 };
 
-canvas.onmousemove = function(event) { 
-    mouse.x = event.x - this.offsetLeft; 
-    mouse.y = event.y - this.offsetTop;
-    console.log(mouse.x);
-}
-function createAsteroid(){
 
-    let asteroid1 = document.getElementById('asteroid1');
-    let asteroid2 = document.getElementById('asteroid2');
-    let asteroids = [ asteroid1, asteroid2 ];
-    let randomAsteroid = asteroids[Math.floor(Math.random() * asteroids.length)];
-    let posX = Math.floor(Math.random() * 770) + 1;
-    target = [ posX, -15 ];
-    targets.push(target)
-
-    ctx.drawImage(randomAsteroid, posX, 1, randomAsteroid.width * 1.6, randomAsteroid.height * 1.6);
-}
-
-function generateAsteroid(nbr){
-
-    for ( let i = 0; i < nbr; i++)
-    {
-        createAsteroid();
+class sprites {
+    constructor(image, posX, posY) {
+        this.image = document.getElementById(image);
+        this.posX = posX;
+        this.posY = posY;
+    }
+    draw(){
+        ctx.drawImage(this.image, this.posX, this.posY);
     }
 }
 
-function createShip(){
-        
-    let spaceship = document.getElementById('spaceship');
-    let flame1 = document.getElementById('flame1');
-    ctx.drawImage(flame1, 300, 420, spaceship.width / 4, spaceship.height / 4);
-    ctx.drawImage(spaceship, 300, 380, spaceship.width / 4, spaceship.height / 4);
+canvas.onmousemove = function(event) { 
+    mouse.x = event.x - this.offsetLeft; 
+    mouse.y = event.y - this.offsetTop;
 }
 
-function createBullet(){
+let gameBackground = new sprites('background', 0, -3000)
+function scrollBackground(gameBackground){
+    let ctx = canvas.getContext("2d");
+    if (gameBackground.posY > 0){
+        gameBackground.posY = -3000;
+    }
+    gameBackground.posY += 2;
+    ctx.drawImage(this.image, this.posX, this.posY);
+    console.log(gameBackground, gameBackground.posX, gameBackground.posY); 
+    console.log(gameBackground);
 
-    let bullet = document.getElementById('laserSmall');
-    ctx.drawImage(bullet, 20, 50, bullet.width / 1.8, bullet.height / 1.8);
+    requestAnimationFrame(scrollBackground);
 }
 
-function createBackground(){
 
-    let background = document.getElementById('background');
-    ctx.drawImage(background, 0, backgroundPos);
-}
 
-function scrollBackground(){
-    background.requestAnimationFrame(scrollBackground); 
-    ctx.clearRect(0,0,canvas.width,canvas.height); 
-    backgroundPos+=2;
-}
 
 window.onload = () =>{
-    
-    let bullet = document.getElementById('laserSmall');
-    ctx.drawImage(bullet, 20, 50, bullet.width / 1.8, bullet.height / 1.8);
     ctx.imageSmoothingEnabled = true;
     ctx.imageSmoothingQuality = 'high';
-    createBackground();
-    generateAsteroid(4);
-    createShip();
-    createBullet();
-}
 
-ctx.stroke();
-console.log(targets);
+    // let gameBackground = new sprites('background', 0, -3000);
+    ctx.drawImage(gameBackground.image, gameBackground.posX, gameBackground.posY);
+
+    setInterval(scrollBackground(gameBackground), 3000);
+
+    // setInterval(scrollBackground(gameBackground), 3000);
+                            
+
+
+                        // var mousedownID = -1;  //Global ID of mouse down interval
+                        // function mousedown(event) {
+                        // if(mousedownID==-1)  //Prevent multimple loops!
+                        //     mousedownID = setInterval(whilemousedown, 1 /*execute every 100ms*/);
+                        
+                        
+                        // }
+                        // function mouseup(event) {
+                        // if(mousedownID!=-1) {  //Only stop if exists
+                        //     clearInterval(mousedownID);
+                        //     mousedownID=-1;
+                        // }
+                        
+                        // }
+                        // function whilemousedown() {
+                        //     if (gameBackground.posY > 0){
+                        //         gameBackground.posY = -3000;
+                        //     }
+                        //     gameBackground.posY += 10;
+                        //     gameBackground.draw();
+                        //     console.log(gameBackground, gameBackground.posX, gameBackground.posY); 
+                        // }
+                        // //Assign events
+                        // document.addEventListener("mousedown", mousedown);
+                        // document.addEventListener("mouseup", mouseup);
+                        // //Also clear the interval when user leaves the window with mouse
+                        // document.addEventListener("mouseout", mouseup);
+
+
+
+
+
+
+
+    console.log(gameBackground);
+
+    let vaisseau = new sprites('spaceship', 300, 380);
+    ctx.drawImage(vaisseau.image, vaisseau.posX, vaisseau.posY, vaisseau.image.width / 4, vaisseau.image.height / 4);
+
+
+    let flame = new sprites('flame1', vaisseau.posX, vaisseau.posY+35);
+    ctx.drawImage(flame.image, flame.posX, flame.posY, vaisseau.image.width / 4, vaisseau.image.height / 4);
+    
+
+    let bullet1 = new sprites('laserSmall', vaisseau.posX+30, vaisseau.posY-25);
+    ctx.drawImage(bullet1.image, bullet1.posX, bullet1.posY, bullet1.image.width / 1.8, bullet1.image.height / 1.8);
+
+    return gameBackground, vaisseau, flame, bullet1;
+}
 
