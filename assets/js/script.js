97ae1b8c571647ba530efcf5945f2f8ca57e95fd
fourneauxthibaut <fourneaux.thibaut@gmail.com>
2021-10-08 17:08:30 +0200

    need to make: making bullet move, put background on back, create target

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
+/**================================================================================================
+ *                                         GLobal Variables
+ *================================================================================================**/
 let canvas = document.getElementById("mon_canvas");
 let ctx = canvas.getContext("2d");
 let gameOver = false;
 let backgroundPos = -3000;
+let i = 0;
 let mouse = {
     x: undefined, 
     y:undefined
 };
-
-
-class sprites {
+class Sprites {
     constructor(image, posX, posY) {
         this.image = document.getElementById(image);
         this.posX = posX;
@@ -18,88 +20,72 @@ class sprites {
         ctx.drawImage(this.image, this.posX, this.posY);
     }
 }
+class Player extends Sprites {
+    draw() {
+        ctx.drawImage(this.image, this.posX, this.posY, this.image.width / 4, this.image.height / 4);
+    }
+}
+/**================================================================================================
+ *                                          Global Object
+ *================================================================================================**/
+let gameBackground = new Sprites('background', 0, -3000);
+let vaisseau = new Player('spaceship', 300, 380);
+let flame = new Player('flame1', vaisseau.posX, vaisseau.posY+35);
+let bullet = new Sprites('laserSmall', vaisseau.posX+30, vaisseau.posY-25);
 
 canvas.onmousemove = function(event) { 
     mouse.x = event.x - this.offsetLeft; 
     mouse.y = event.y - this.offsetTop;
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    vaisseau.posX = mouse.x-( ( vaisseau.image.width / 4 ) / 2 ) - 2;
+    flame.posX = mouse.x-( ( vaisseau.image.width / 4 ) / 2 ) - 2;
+    vaisseau.draw();
+    flame.draw();
+
+    canvas.onclick = function(event) { 
+        ctx.drawImage(bullet.image, mouse.x-( vaisseau.image.width / 14 ), bullet.posY, bullet.image.width / 1.8, bullet.image.height / 1.8);
+    }
 }
 
-let gameBackground = new sprites('background', 0, -3000)
 function scrollBackground(){
     if (gameBackground.posY > 0){
         gameBackground.posY = -3000;
     }
-    gameBackground.posY += 2;
+    gameBackground.posY += 1;
     gameBackground.draw();
-    console.log(gameBackground, gameBackground.posX, gameBackground.posY); 
-    console.log(gameBackground);
 
-    requestAnimationFrame(scrollBackground);
+    if ( !gameOver ){
+        requestAnimationFrame(scrollBackground);
+    }
 }
-
-
-
+function moveFlame(){
+    if ( i % 2 == 0)
+    {
+        flame.image = document.getElementById('flame2');
+        if ( !gameOver ){
+            requestAnimationFrame(moveFlame);
+        }
+    }
+    else
+    {
+        flame.image = document.getElementById('flame1');
+        if ( !gameOver ){
+            requestAnimationFrame(moveFlame);
+        }
+    }
+    i++;
+}
 
 window.onload = () =>{
     ctx.imageSmoothingEnabled = true;
     ctx.imageSmoothingQuality = 'high';
 
-    // let gameBackground = new sprites('background', 0, -3000);
     ctx.drawImage(gameBackground.image, gameBackground.posX, gameBackground.posY);
-
     setInterval(scrollBackground(), 3000);
+    setInterval(moveFlame(), 3000);
 
-    // setInterval(scrollBackground(gameBackground), 3000);
-                            
-
-
-                        // var mousedownID = -1;  //Global ID of mouse down interval
-                        // function mousedown(event) {
-                        // if(mousedownID==-1)  //Prevent multimple loops!
-                        //     mousedownID = setInterval(whilemousedown, 1 /*execute every 100ms*/);
-                        
-                        
-                        // }
-                        // function mouseup(event) {
-                        // if(mousedownID!=-1) {  //Only stop if exists
-                        //     clearInterval(mousedownID);
-                        //     mousedownID=-1;
-                        // }
-                        
-                        // }
-                        // function whilemousedown() {
-                        //     if (gameBackground.posY > 0){
-                        //         gameBackground.posY = -3000;
-                        //     }
-                        //     gameBackground.posY += 10;
-                        //     gameBackground.draw();
-                        //     console.log(gameBackground, gameBackground.posX, gameBackground.posY); 
-                        // }
-                        // //Assign events
-                        // document.addEventListener("mousedown", mousedown);
-                        // document.addEventListener("mouseup", mouseup);
-                        // //Also clear the interval when user leaves the window with mouse
-                        // document.addEventListener("mouseout", mouseup);
-
-
-
-
-
-
-
-    console.log(gameBackground);
-
-    let vaisseau = new sprites('spaceship', 300, 380);
     ctx.drawImage(vaisseau.image, vaisseau.posX, vaisseau.posY, vaisseau.image.width / 4, vaisseau.image.height / 4);
-
-
-    let flame = new sprites('flame1', vaisseau.posX, vaisseau.posY+35);
     ctx.drawImage(flame.image, flame.posX, flame.posY, vaisseau.image.width / 4, vaisseau.image.height / 4);
-    
-
-    let bullet1 = new sprites('laserSmall', vaisseau.posX+30, vaisseau.posY-25);
-    ctx.drawImage(bullet1.image, bullet1.posX, bullet1.posY, bullet1.image.width / 1.8, bullet1.image.height / 1.8);
-
-    return gameBackground, vaisseau, flame, bullet1;
 }
 
