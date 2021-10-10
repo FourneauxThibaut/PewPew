a6e59b830972a5c91ebb0251fcf12a5ec4eb5a4f
fourneauxthibaut <fourneaux.thibaut@gmail.com>
2021-10-10 03:05:46 +0200

    rebuilding and adding shoot system

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,29 @@
 /**================================================================================================
- *                                         GLobal Variables
+ **                                        Canvas Setup
  *================================================================================================**/
-let canvas = document.getElementById("mon_canvas");
-let ctx = canvas.getContext("2d");
+let canvas = document.querySelector('#mon_canvas');
+let ctx = canvas.getContext('2d');
+let width = canvas.width;
+let height = canvas.height;
 let gameOver = false;
-let backgroundPos = -3000;
-let i = 0;
+let count = 0;
 let mouse = {
     x: undefined, 
     y:undefined
 };
+/**================================================================================================
+ **                                         Classes
+ *?  Create Classes
+ *@param Sprites
+ *@param Player
+ *@param Bullet
+ *================================================================================================**/
 class Sprites {
     constructor(image, posX, posY) {
         this.image = document.getElementById(image);
         this.posX = posX;
         this.posY = posY;
+        this.alive = true;
     }
     draw(){
         ctx.drawImage(this.image, this.posX, this.posY);
@@ -25,68 +34,76 @@ class Player extends Sprites {
         ctx.drawImage(this.image, this.posX, this.posY, this.image.width / 4, this.image.height / 4);
     }
 }
-class Bullets extends Sprites {
+class Background extends Sprites {
     draw() {
-        ctx.drawImage(this.image, mouse.x-( vaisseau.image.width / 14 ), this.posY, this.image.width / 1.8, this.image.height / 1.8);
+        ctx.drawImage(this.image, this.posX, this.posY);
+    }
+    update() {
+        if (this.posY > 0){
+            gameBackground.posY = -3000;
+        }
+        this.posY += 0.6;
     }
 }
+class Bullet extends Sprites {
+    init() {
+        ctx.drawImage(this.image, this.posX, this.posY, this.image.width / 1.8, this.image.height / 1.8);
+    }
+    draw() {
+        if (this.alive){
+            if ( this.image.id == "laser" ){
+                ctx.drawImage(this.image, this.posX-7, this.posY, this.image.width / 1.8, this.image.height / 1.8);
+            }
+            else if ( this.image.id == "laserSmall"){
+                ctx.drawImage(this.image, this.posX, this.posY, this.image.width / 1.8, this.image.height / 1.8);
+            }
+        }
+    }
+    update() {
+        if (this.alive){
+            if ( this.posY <= 300 )
+            {
+                this.image = document.getElementById('laser');
+            }
+            // update the x value only if the object is alive
+            this.posY -= 5;
+            // Check if the bullet is on screen
+            if (this.posY <= -100) {
+                this.alive = false;
+            }
+        }
+    }
+}
+
 /**================================================================================================
- *                                          Global Object
+ **                                      Sprites & Obj
  *================================================================================================**/
-let gameBackground = new Sprites('background', 0, -3000);
-let vaisseau = new Player('spaceship', 300, 380);
-let flame = new Player('flame1', vaisseau.posX, vaisseau.posY+35);
-let bullet = new Bullets('laserSmall', vaisseau.posX+30, vaisseau.posY-25);
-
-function updateScreen(){
-    gameBackground.draw();
-    vaisseau.draw();
-    flame.draw();
+    let gameBackground = new Background('background', 0, -3000);
+    let vaisseau = new Player('spaceship', 300, 380);
+    let flame = new Player('flame1', vaisseau.posX, vaisseau.posY+35);
+    let bullets = [];
+ 
+/**================================================================================================
+ **                                      CreateBullet
+ *?  initialise bullet on click
+ *
+ *================================================================================================**/
+function createBullet() {
+    bullet = new Bullet('laserSmall', mouse.x-36, 380);
+    bullets.push(bullet);
+    bullet.init();
 }
 
-function scrollBackground(){
-    if (gameBackground.posY > 0){
-        gameBackground.posY = -3000;
-    }
-    gameBackground.posY += 0.6;
-    updateScreen();
-
-    if ( !gameOver ){
-        requestAnimationFrame(scrollBackground);
-    }
-}
-window.onload = () =>{
-    ctx.imageSmoothingEnabled = true;
-    ctx.imageSmoothingQuality = 'high';
-
-    ctx.drawImage(gameBackground.image, gameBackground.posX, gameBackground.posY);
-    setInterval(scrollBackground(), 1);
-    moveFlame();
-
-}
-
-
-canvas.onmousemove = function(event) { 
-    mouse.x = event.x - this.offsetLeft; 
-    mouse.y = event.y - this.offsetTop;
-    
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    vaisseau.posX = mouse.x-( ( vaisseau.image.width / 4 ) / 2 ) - 2;
-    flame.posX = mouse.x-( ( vaisseau.image.width / 4 ) / 2 ) - 2;
-    vaisseau.draw();
-    flame.draw();
-
-    canvas.onclick = function(event) { 
-        updateScreen();
-        bullet.draw();
-    }
-}
-
+/**================================================================================================
+ **                                      Move Flame
+ *?  Flame animation with 2 image 
+ *
+ *================================================================================================**/
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 async function moveFlame(){
-    if ( i % 2 == 0)
+    if ( count % 2 == 0)
     {
         flame.image = document.getElementById('flame2');
         await sleep(80);
@@ -97,12 +114,74 @@ async function moveFlame(){
     else
     {
         flame.image = document.getElementById('flame1');
-        await sleep(80);
+        await sleep(700);
         if ( !gameOver ){
             requestAnimationFrame(moveFlame);
         }
     }
-    i++;
+    count++;
 }
 
+/**================================================================================================
+ **                                         UpdateScreen
+ *?  update all sprites in the list
+ *
+ *================================================================================================**/
+function updateScreen() {
+    if ( !gameOver )
+    {
+        gameBackground.update();
+        for (let i = bullets.length; i--;) 
+        {
+            bullets[i].update();
+        }
+        moveFlame();
+    }
+}
 
+/**================================================================================================
+ **                                         Render
+ *?  reset and draw all element
+ *
+ *================================================================================================**/
+function render() {
+  // Here we render all the sprites after clearing the screen
+  ctx.clearRect(0, 0, width, height);
+  gameBackground.draw();
+  for (let j = 0; j < bullets.length; j++) {
+    bullets[j].draw();
+  }
+  vaisseau.draw();
+  flame.draw();
+}
+
+/**================================================================================================
+ **                                     Event Listener
+ *
+ *================================================================================================**/
+canvas.addEventListener('click', function() {
+    createBullet();
+});
+
+canvas.onmousemove = function(event) { 
+    mouse.x = event.x - this.offsetLeft; 
+    mouse.y = event.y - this.offsetTop;
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    vaisseau.posX = mouse.x-( ( vaisseau.image.width / 4 ) / 2 ) - 2;
+    flame.posX = mouse.x-( ( vaisseau.image.width / 4 ) / 2 ) - 2;
+}
+
+/**================================================================================================
+ **                                         Loop
+ *
+ *================================================================================================**/
+function loop() {
+  // Very simple and naive game loop
+  updateScreen();
+  render();
+  requestAnimationFrame(loop);
+}
+
+// Start the game
+requestAnimationFrame(loop);
